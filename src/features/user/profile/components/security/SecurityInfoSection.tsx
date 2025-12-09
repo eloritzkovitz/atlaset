e5138a731282e9ee4b4f7c5aa7a65c4d80d1ce7f
eloritzkovitz <elor2200@gmail.com>
2025-12-09 21:12:54 +0200

    Added end session button

--- src/features/user/profile/components/security/SecurityInfoSection.tsx
+++ src/features/user/profile/components/security/SecurityInfoSection.tsx
@@ -3,7 +3,10 @@ import { useUserDevices } from "@features/user/auth/hooks/useUserDevices";
 import { capitalize } from "@utils/string";
 import { SecurityInfoRow } from "./SecurityInfoRow";
 import { useUserActivity } from "../../hooks/useUserActivity";
-import { FaDesktop, FaMobile, FaTablet } from "react-icons/fa6";
+import { FaDesktop, FaMobile, FaPowerOff, FaTablet } from "react-icons/fa6";
+import { getUserCollection } from "@utils/firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { ActionButton } from "@components";
 
 export function SecurityInfoSection() {
   const { user } = useAuth();
@@ -18,9 +21,15 @@ export function SecurityInfoSection() {
   // Get device icon based on user agent
   function getDeviceIcon(device: any) {
     const ua = device.userAgent || "";
-    if (/mobile/i.test(ua)) return <FaMobile className="mr-2" size={28} />;
-    if (/tablet|ipad/i.test(ua)) return <FaTablet className="mr-2" size={28} />;
-    return <FaDesktop className="mr-2 mb-4" size={64} />;
+    if (/mobile/i.test(ua)) return <FaMobile className="mr-4" size={64} />;
+    if (/tablet|ipad/i.test(ua)) return <FaTablet className="mr-4" size={64} />;
+    return <FaDesktop className="mr-4" size={64} />;
+  }
+
+  // Handle device removal
+  async function handleRemoveDevice(deviceId: string) {
+    const devicesCol = getUserCollection("devices");
+    await deleteDoc(doc(devicesCol, deviceId));
   }
 
   return (
@@ -62,17 +71,30 @@ export function SecurityInfoSection() {
             <SecurityInfoRow
               key={device.id}
               label={
-                <>
+                <span className="flex items-center">
                   {getDeviceIcon(device)}
-                  {device.deviceName || device.userAgent || "Device"}
-                </>
+                  <span className="text-lg">
+                    {device.deviceName || device.userAgent || "Device"}
+                  </span>
+                </span>
               }
               value={
-                device.lastActive
-                  ? `Last active: ${new Date(
-                      device.lastActive
-                    ).toLocaleString()}`
-                  : "Unknown"
+                <div className="flex items-center">
+                  {device.lastActive
+                    ? `Last active: ${new Date(
+                        device.lastActive
+                      ).toLocaleString()}`
+                    : "Unknown"}
+                  <ActionButton
+                    className="py-2 px-4 bg-blue-800 text-white rounded-full hover:bg-blue-700"
+                    icon={<FaPowerOff size={18} />}
+                    title="End this session"
+                    ariaLabel="End session"
+                    onClick={() => handleRemoveDevice(device.id)}
+                  >
+                    End Session
+                  </ActionButton>
+                </div>
               }
             />
           ))
