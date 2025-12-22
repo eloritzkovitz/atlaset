e728f6183b8fe9f74d4b7a531cae1b94535dff93
eloritzkovitz <elor2200@gmail.com>
2025-12-22 23:21:13 +0200

    Small fix

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer({ children }: { children?: React.ReactNode }) {
     <footer className="mt-8 py-4 text-center text-muted text-sm">
       {children}
       <div className="mt-2 mb-1">© {new Date().getFullYear()} Atlaset</div>
-      <a href="https://github.com/eloritzkovitz/atlaset">View on GitHub</a>
+      <a href="https://github.com/eloritzkovitz/atlaset" target="_blank" rel="noopener noreferrer">View on GitHub</a>
     </footer>
   );
 }
