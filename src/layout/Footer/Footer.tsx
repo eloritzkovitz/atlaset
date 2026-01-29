0900671dd46787484e5f610a22c3e6758b2a822f
eloritzkovitz <elor2200@gmail.com>
2026-01-29 12:40:24 +0200

    Show branding in footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer({ children }: { children?: React.ReactNode }) {
     <footer className="bg-surface-alt py-4 text-center text-sm">
       {children}
       <div className="mt-2 mb-1 font-semibold flex items-center justify-center gap-2">
-        <BrandCopyright />
+        <BrandCopyright showLogo={true} />
         <a
           href="/about"
           className="ml-3 !text-muted hover:text-muted/70 underline"
