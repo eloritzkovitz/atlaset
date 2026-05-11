483dc9fabd9f06fb3d728ea1f040c19dd68cce4e
eloritzkovitz <elor2200@gmail.com>
2026-05-11 19:06:35 +0300

    Redesigned about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,47 +1,28 @@
-import { Branding } from "@components";
 import { usePageTitle } from "@hooks";
 
 export default function AboutPage() {
   usePageTitle("About Atlaset | Atlaset");
 
   return (
-    <main className="w-full max-w-2xl mx-auto py-16 px-4 flex flex-col items-center text-center">
-      <div className="flex flex-col items-center gap-3 mb-8">
-        <span className="w-30 h-30 inline-block">
-          <Branding size={120} />
-        </span>
-        <h1 className="text-4xl font-extrabold drop-shadow-sm">
+    <main className="w-full max-w-6xl mx-auto py-16 px-4 flex flex-col text-start">
+      <div className="flex flex-col gap-3 mb-8">
+        <h1
+          className="text-5xl sm:text-6xl md:text-7xl font-bold leading-tight drop-shadow-sm"
+          style={{ fontSize: "6rem" }}
+        >
           About Atlaset
         </h1>
       </div>
-      <p className="text-lg text-muted mb-12 max-w-xl">
-        Welcome to Atlaset, a modern and interactive, fully-configurable country
-        explorer and travel tracker.
+      <p className="text-4xl text-muted mb-12 max-w-4xl">
+        Atlaset came out from a longtime dream of mine. As an avid traveler, I
+        wanted to create a tool that would help me and my family track and
+        revisit our travels in an intuitive and interactive way.
       </p>
-      <div className="w-full flex flex-col gap-6 mb-8 items-center text-center">
-        <section>
-          <h2 className="text-2xl font-bold mb-2">Mission</h2>
-          <p className="text-lg text-muted mb-12">
-            As an avid traveler, I created Atlaset to document my own journeys
-            in a fun and engaging way.
-          </p>
-        </section>
-        <section>
-          <h2 className="text-2xl font-bold mb-2">Open Source</h2>
-          <p className="text-lg text-muted mb-12">
-            Atlaset is open source and available on{" "}
-            <a
-              href="https://github.com/eloritzkovitz/atlaset"
-              className="text-primary underline"
-            >
-              GitHub
-            </a>
-            . Feedback and ideas are always welcome!
-          </p>
-        </section>
-      </div>
-      <div className="text-lg text-muted text-center mt-8">
-        Made by{" "}
+      <p className="text-4xl text-muted mb-12 max-w-4xl">
+        With Atlaset, you can have your own personalized experience!
+      </p>
+      <p className="text-4xl text-muted mb-12 max-w-4xl">
+        Created by{" "}
         <a
           href="https://github.com/eloritzkovitz"
           className="underline hover:text-primary"
@@ -50,7 +31,7 @@ export default function AboutPage() {
         >
           Elor Itzkovitz
         </a>
-      </div>
+      </p>
     </main>
   );
 }
