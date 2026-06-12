5255e5508fa3bca2faa7236aea7426e220c8068f
eloritzkovitz <elor2200@gmail.com>
2026-06-12 12:40:41 +0300

    Added translations

--- src/shared/components/navigation/AuthButtons.tsx
+++ src/shared/components/navigation/AuthButtons.tsx
@@ -1,8 +1,10 @@
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { ActionButton } from "../action/ActionButton";
 
 export function AuthButtons() {
   const navigate = useNavigate();
+  const { t } = useTranslation("auth");
 
   return (
     <div className="flex justify-end gap-2">
@@ -11,14 +13,14 @@ export function AuthButtons() {
         className="min-w-[100px] !rounded-full"
         onClick={() => navigate("/login")}
       >
-        Login
+        {t("login.signinButton", "Log in")}
       </ActionButton>
       <ActionButton
         variant="primary"
         className="min-w-[100px] !rounded-full"
         onClick={() => navigate("/signup")}
       >
-        Sign up
+        {t("login.signupButton", "Sign up")}
       </ActionButton>
     </div>
   );
