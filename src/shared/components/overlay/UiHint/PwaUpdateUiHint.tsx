59d6feb61ab93e5fd5015153432dcf6b02daa708
eloritzkovitz <elor2200@gmail.com>
2026-07-11 22:44:31 +0300

    Fixed more translations

--- src/shared/components/overlay/UiHint/PwaUpdateUiHint.tsx
+++ src/shared/components/overlay/UiHint/PwaUpdateUiHint.tsx
@@ -1,17 +1,20 @@
+import { useTranslation } from "react-i18next";
 import { FaArrowsRotate } from "react-icons/fa6";
 import { usePwaUpdate, useUiHint } from "@hooks";
 
 export function PwaUpdateUiHint() {
   const { needRefresh, updateServiceWorker } = usePwaUpdate();
+  const { t } = useTranslation("common");
 
   useUiHint(
     needRefresh
       ? {
           message: (
             <>
-              A new version is available.{" "}
+              {t("pwa.updateAvailable", "A new version is available.")}
+              {""}
               <button className="underline" onClick={updateServiceWorker}>
-                Update now
+                {t("pwa.updateNow", "Update now")}
               </button>
             </>
           ),
@@ -19,7 +22,7 @@ export function PwaUpdateUiHint() {
         }
       : null,
     0,
-    { key: "pwa-update", dismissable: true }
+    { key: "pwa-update", dismissable: true },
   );
 
   return null;
