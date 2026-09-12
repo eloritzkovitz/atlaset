cc11f1dbb3c233cd35ef2fdaf95442414335f4cf
eloritzkovitz <elor2200@gmail.com>
2026-09-12 10:10:04 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,12 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      registerType: "prompt",
+      registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       workbox: {
         cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
         dontCacheBustURLsMatching: /\.[a-f0-9]{8}\./,
         maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
       },
